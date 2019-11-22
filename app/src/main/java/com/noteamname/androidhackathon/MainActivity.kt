a2daf5a6e9f6ea13fcd7565c977aa12a39d15dcc
Artem Glotov <glotovartemalex@gmail.com>
2019-11-22 22:01:27 +0300

    Add toilet roll view

--- app/src/main/java/com/noteamname/androidhackathon/MainActivity.kt
+++ app/src/main/java/com/noteamname/androidhackathon/MainActivity.kt
@@ -2,13 +2,27 @@ package com.noteamname.androidhackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.addCallback
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onBackPressedDispatcher.addCallback(this) {
+            navController.popBackStack() || super.onSupportNavigateUp() || run {
+                finish()
+                true
+            }
+        }
     }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 }
